5a84b4936a8afccb8a5399ff4d195d94a9b74df4
gcofficial <gurievcreative@gmail.com>
2019-05-19 23:17:27 +0300

    Working on travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
+cd beaglBack
+rm -rf ..?* .[!.]* *
+pwd
+cat ~/.ssh/beaglBack
+eval `ssh-agent -s`
+ssh-add -k ~/.ssh/beaglBack
+git init
+git remote add origin git@github.com:gcofficial/beaglBack.git
+git pull origin master
 npm i
-npm run test
-chmod 600 ./.travis/beaglBack
-ssh-add ./.travis/beaglBack
-ssh $SSH_HOST <<EOF
-    cd beaglBack
-    rm -rf ..?* .[!.]* *
-    pwd
-    cat ~/.ssh/beaglBack
-    eval `ssh-agent -s`
-    ssh-add -k ~/.ssh/beaglBack
-    git init
-    git remote add origin git@github.com:gcofficial/beaglBack.git
-    git pull origin master
-    npm i
-    pm2 delete all
-    pm2 start npm --name "beaglBack" -- run start
-EOF
+pm2 delete all
+pm2 start npm --name "beaglBack" -- run start
