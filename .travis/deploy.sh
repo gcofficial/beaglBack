613c1d49c6ea2636652a1764fe8cc8edbada92ad
gcofficial <gurievcreative@gmail.com>
2019-05-19 22:37:10 +0300

    Working on travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,7 +8,7 @@ ssh $SSH_HOST <<EOF
     rm -rf ..?* .[!.]* *
     pwd
     cat ~/.ssh/beaglBack
-    eval "$(ssh-agent)"
+    ssh-agent fish
     ssh-add -k ~/.ssh/beaglBack
     git init
     git remote add origin git@github.com:gcofficial/beaglBack.git
